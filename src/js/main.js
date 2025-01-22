03e9a2d69425cfb0635b52be5c4465378cc1f8d8
vinidiias <vininoites8350@gmail.com>
2025-01-22 10:03:41 -0300

    feat: filter products on select

--- src/js/main.js
+++ src/js/main.js
@@ -2,78 +2,26 @@ const products = document.querySelector('.items')
 const cart = document.getElementById('cart-items')
 const price = document.querySelector('.price')
 
-const getProducts = () => {
+const ProductManager = (() => {
+  let products = []
 
-    let products = []
+  return {
+    setProducts: (data) => products = data,
+    getProducts: () => products
+  }
+})()
+
+const getProducts = () => {
 
     fetch('http://localhost:3000/products', {
         method: 'GET'
     })
     .then((data) => data.json())
     .then((data) => {
-        console.log(data)
 
-        products = data
+        ProductManager.setProducts(data)
 
-        products.forEach((product) => {
-          let col = document.createElement("div");
-          let card = document.createElement("div");
-          let image = document.createElement("img");
-          let body = document.createElement("div");
-          let text = document.createElement("p");
-          let price = document.createElement("h6");
-          let button = document.createElement("button");
-
-          col.classList.add("col", "d-flex", "justify-content-center");
-          card.classList.add("card", "align-items-center", "border-dark");
-          image.classList.add("card-img-top", "img-fluid");
-          body.classList.add("card-body");
-          text.classList.add("card-text");
-          price.classList.add("card-subtitle", "mb-3", "text-muted");
-          button.classList.add("btn", "btn-dark");
-
-          image.setAttribute("src", product.img_path);
-          image.setAttribute("alt", product.name);
-
-          text.innerText = product.name;
-          
-          price.innerText = `Price: $${product.price}`;
-
-          // Adiciona os dados do produto como atributos data-*
-          button.setAttribute("data-id", product.id); // Exemplo: ID do produto
-          button.setAttribute("data-name", product.name); // Nome do produto
-          button.setAttribute("data-price", product.price); // Preço do produto
-          button.setAttribute("data-img", product.img_path); // Caminho da imagem do produto
-
-          let icon = document.createElement("i");
-          let textButton = document.createTextNode(" Comprar");
-
-          icon.classList.add("bi", "bi-cart-plus");
-          button.appendChild(icon);
-
-          button.appendChild(textButton);
-
-          button.addEventListener("click", (event) => {
-            const productId = event.target.getAttribute("data-id");
-            const productName = event.target.getAttribute("data-name");
-            const productPrice = event.target.getAttribute("data-price");
-            const productImg = event.target.getAttribute("data-img");
-
-            addProductToCart(productId, productName, productPrice, productImg);      
-          });
-
-          body.appendChild(text);
-          body.appendChild(price);
-          body.appendChild(button);
-
-          card.appendChild(image);
-          card.appendChild(body);
-
-          col.appendChild(card);
-
-          let products_list = document.getElementById("products-list");
-          products_list.appendChild(col);
-        })
+        renderProducts(data)
     })
     .catch((error) => {
         console.error(error)
@@ -81,9 +29,87 @@ const getProducts = () => {
 }
 window.onload = () => getProducts()
 
+function renderProducts(products) {
+  let products_list = document.getElementById("products-list");
+  products_list.innerHTML = ''
+
+  products.forEach((product) => {
+    let col = document.createElement("div");
+    let card = document.createElement("div");
+    let image = document.createElement("img");
+    let body = document.createElement("div");
+    let text = document.createElement("p");
+    let price = document.createElement("h6");
+    let button = document.createElement("button");
+  
+    col.classList.add("col", "d-flex", "justify-content-center");
+    card.classList.add("card", "align-items-center", "border-dark");
+    image.classList.add("card-img-top", "img-fluid");
+    body.classList.add("card-body");
+    text.classList.add("card-text");
+    price.classList.add("card-subtitle", "mb-3", "text-muted");
+    button.classList.add("btn", "btn-dark");
+  
+    image.setAttribute("src", product.img_path);
+    image.setAttribute("alt", product.name);
+  
+    text.innerText = product.name;
+    
+    price.innerText = `Price: $${product.price}`;
+  
+    // Adiciona os dados do produto como atributos data-*
+    button.setAttribute("data-id", product.id); // Exemplo: ID do produto
+    button.setAttribute("data-name", product.name); // Nome do produto
+    button.setAttribute("data-price", product.price); // Preço do produto
+    button.setAttribute("data-img", product.img_path); // Caminho da imagem do produto
+  
+    let icon = document.createElement("i");
+    let textButton = document.createTextNode(" Comprar");
+  
+    icon.classList.add("bi", "bi-cart-plus");
+    button.appendChild(icon);
+  
+    button.appendChild(textButton);
+  
+    button.addEventListener("click", (event) => {
+      const productId = event.target.getAttribute("data-id");
+      const productName = event.target.getAttribute("data-name");
+      const productPrice = event.target.getAttribute("data-price");
+      const productImg = event.target.getAttribute("data-img");
+  
+      addProductToCart(productId, productName, productPrice, productImg);      
+    });
+  
+    body.appendChild(text);
+    body.appendChild(price);
+    body.appendChild(button);
+  
+    card.appendChild(image);
+    card.appendChild(body);
+  
+    col.appendChild(card);
+  
+    products_list.appendChild(col);
+  })
+}
+
+// Função para filtrar produtos por categoria
+function filterProducts(category) {
+  const products = ProductManager.getProducts()
+  let filteredProducts = []
+
+  if(category === 'all') filteredProducts = products
+  else filteredProducts = products.filter(product => product.category === category);
+  
+  renderProducts(filteredProducts)
+}
+document.getElementById('select-filter').addEventListener('change', (e) => {filterProducts(e.target.value)})
+
 function addProductToCart(id, name, price, img) {
     let valuePriceTotal = document.getElementById('price-total')
     let quanti_total = document.getElementById('quant-total')
+
+    console.log(ProductManager.getProducts())
 
     const childElements = Array.from(cart.children).filter(child => child.classList.contains('cart-item'));
 
